f8eca977c6b2343d3a9dde3eb5c53eb8df7fb46b
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2014-12-15 17:26:39 +0100

    Rebase from 'rpm/indigo/cob_light'

--- ros/scripts/test_sprofi_modes.py
+++ ros/scripts/test_sprofi_modes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #***************************************************************
 #
-# Copyright (c) 2010
+# Copyright (c) 2014
 #
 # Fraunhofer Institute for Manufacturing Engineering	
 # and Automation (IPA)
@@ -14,11 +14,10 @@
 #								
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #			
-# Author: Florian Weisshardt, email:florian.weisshardt@ipa.fhg.de
-# Supervised by: Florian Weisshardt, email:florian.weisshardt@ipa.fhg.de
+# Author: Thiago de Freitas, email:tdf@ipa.fhg.de
+# Supervised by: Thiago de Freitas, email:tdf@ipa.fhg.de
 #
-# Date of creation: June 2010
-# ToDo:
+# Date of creation: October 2014
 #
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
@@ -56,48 +55,101 @@ import roslib
 roslib.load_manifest('cob_light')
 import rospy
 from std_msgs.msg import ColorRGBA
+from cob_light.srv import *
+from cob_light.msg import *
 
 def changeColor():
-	pub = rospy.Publisher('light_controller/command', ColorRGBA, queue_size=1)
-	rospy.init_node('light_test')
-	#color in rgb color space ranging from 0 to 1
-	red = ColorRGBA()
-	red.r = 1
-	red.g = 0
-	red.b = 0
-	red.a = 1
-	
-	yellow = ColorRGBA()
-	yellow.r = 0.4
-	yellow.g = 1
-	yellow.b = 0
-	yellow.a = 1
-	
-	green = ColorRGBA()
-	green.r = 0
-	green.g = 1
-	green.b = 0
-	green.a = 1
-	
-	blue = ColorRGBA()
-	blue.r = 0
-	blue.g = 0
-	blue.b = 1
-	blue.a = 1
-	
-	white = ColorRGBA()
-	white.r = 0.3
-	white.g = 1
-	white.b = 0.3
-	white.a = 1
-	
-	for color in [red,yellow,green,white,blue,green]:
-		rospy.loginfo("Setting rgb to %s [%d, %d, %d]",color.r,color.g,color.b,color.a)
-		pub.publish(color)
-		time.sleep(3)
+  rospy.wait_for_service('/light_controller/mode')
+  control_lights = rospy.ServiceProxy('/light_controller/mode', SetLightMode)
+  rospy.init_node('light_test')
+  light_mode = LightMode()
+  #color in rgb color space ranging from 0 to 1
+  red = ColorRGBA()
+  red.r = 1
+  red.g = 0
+  red.b = 0
+  red.a = 0.7
+  
+  light_red = ColorRGBA()
+  light_red.r = 1
+  light_red.g = 0
+  light_red.b = 0
+  light_red.a = 0.3
+  
+  off = ColorRGBA()
+  off.r = 1
+  off.g = 0
+  off.b = 0
+  off.a = 0.01
+  
+  dimm = ColorRGBA()
+  dimm.r = 1
+  dimm.g = 0
+  dimm.b = 0
+  dimm.a = 0.04
+
+  yellow = ColorRGBA()
+  yellow.r = 1
+  yellow.g = 1
+  yellow.b = 0
+  yellow.a = 1
+
+  green = ColorRGBA()
+  green.r = 0
+  green.g = 1
+  green.b = 0
+  green.a = 1
+
+  blue = ColorRGBA()
+  blue.r = 0
+  blue.g = 1
+  blue.b = 0.7
+  blue.a = 0.4
+
+  white = ColorRGBA()
+  white.r = 0.3
+  white.g = 1
+  white.b = 0.3
+  white.a = 1
+
+  light_mode.mode = 6
+  light_mode.frequency=100
+  seq1 = Sequence()
+  seq1.color = blue
+  seq1.hold_time = 4;
+  seq1.cross_time = 1;
+  light_mode.sequences.append(seq1);
+  
+  seq2 = Sequence()
+  seq2.color = red;
+  seq2.hold_time = 4;
+  seq2.cross_time = 1;  
+  light_mode.sequences.append(seq2);
+  
+ # seq3 = Sequence()
+ # seq3.color = off;
+ # seq3.hold_time = 0.06;
+ # seq3.cross_time = 0.05;
+ # light_mode.sequences.append(seq3);
+  
+ # seq4 = Sequence()
+ # seq4.color = light_red;
+ # seq4.hold_time = 0.08;
+ # seq4.cross_time = 0.05;
+ # light_mode.sequences.append(seq4);
+ # 
+  try:
+    resp1 = control_lights(light_mode)
+    print resp1
+  except rospy.ServiceException, e:
+    print "Service call failed: %s"%e
+      
+  time.sleep(6)
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('light_test')
         changeColor()
-    except rospy.ROSInterruptException: pass
+    except rospy.ROSInterruptException: 
+        pass
 
